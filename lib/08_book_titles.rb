7770c9ec0faabfc6944f0d7efa694ab5caedd01f
Vannak Chhay <vannak.chhay1@gmail.com>
2017-01-20 18:20:07 -0500

    finished book_titles.rb

--- lib/08_book_titles.rb
+++ lib/08_book_titles.rb
@@ -1,3 +1,27 @@
 class Book
-  # TODO: your code goes here!
+  def title
+    @title
+  end
+
+  def title=(title)
+    @title = titleize(title)
+  end
+
+  def titleize(title)
+    list_of_exclusion = ["the", "a", "an", "and", "in", "of"]
+
+    words = title.split(" ")
+    results = words.collect do |word|
+      if list_of_exclusion.include?(word)
+        word
+      else
+        word.capitalize
+      end
+    end
+
+    #handles exceptions being first word
+    results[0].capitalize!
+
+    results.join(" ")
+  end
 end
