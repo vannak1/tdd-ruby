768bcc58cf0b28daf6b8eae52b206219c47852e4
Vannak Chhay <vannak.chhay1@gmail.com>
2017-01-20 22:18:36 -0500

    finished dict

--- lib/11_dictionary.rb
+++ lib/11_dictionary.rb
@@ -1,3 +1,46 @@
 class Dictionary
-  # TODO: your code goes here!
+  def initialize
+    @entries = Hash.new
+  end
+
+  def entries
+    @entries ||= {}
+  end
+
+  def add(arg)
+    if arg.class == String
+      entries[arg] = nil
+    else
+      arg.each do |k,v|
+        entries[k] = v
+      end
+    end
+  end
+
+  def keywords
+    @entries.keys.sort
+  end
+
+  def find(keyword)
+    results = {}
+    entries.each do |key, defin|
+      results[key] = defin if key =~ /^#{keyword}/
+    end
+
+    results
+  end
+
+  def include?(keyword)
+    @entries.has_key?(keyword)
+  end
+
+  def printable
+    str = []
+
+    entries.sort.each do |k, v|
+      str << "[#{k}] \"#{v}\""
+    end
+
+    str.join("\n")
+  end
 end
